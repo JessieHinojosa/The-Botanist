ca1beeae645d13b306196e3671da07e1ade4223e
Kayla Dombrosky <kdombroskyy@live.com>
2021-09-28 20:10:59 -0400

    atlas connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -19,9 +19,10 @@
 // module.exports = connectDB;
 
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/botanistDB',
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
