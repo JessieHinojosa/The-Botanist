630688ba1af52491799bd83f64f160b0cd5b3437
Kayla Dombrosky <kdombroskyy@live.com>
2021-09-28 20:16:27 -0400

    move .env

--- server/config/connection.js
+++ server/config/connection.js
@@ -21,14 +21,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(
-  process.env.MONGODB_URI,
-  {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect(process.env.MONGODB_URI , {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 module.exports = mongoose.connection;
